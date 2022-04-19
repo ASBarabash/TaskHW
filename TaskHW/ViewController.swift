625d18eeb32a8296aa3b2925b8598fc53f2c591d
Alexander Barabash <alexanderbarabash86@gmail.com>
2022-04-19 20:26:49 +0300

    Initial Commit

---
+++ TaskHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskHW
+//
+//  Created by Alexandr Barabash on 19.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
